66ddb7e62670a510800c4fd52b1d03a7d30ed840
sarah semark <sarah@octopusthink.com>
2021-01-13 21:53:34 +0000

    feat: Set up slightly more decorative footer.

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,21 +1,56 @@
 import { css } from '@emotion/core';
-import { Paragraph, Link } from '@octopusthink/nautilus';
+import { Paragraph, Link, heading, useTheme } from '@octopusthink/nautilus';
 import React from 'react';
 
 import config from 'data/SiteConfig';
 
 const SiteFooter = () => {
+  const theme = useTheme();
+
   return (
     <footer
       css={css`
+        width: 100%;
         margin: 0 auto;
-        max-width: ${config.contentWidth};
-        max-width: ${config.siteContentWidth};
         padding: 8rem 0 1.6rem;
         text-align: center;
+        position: relative;
+
+        &::before {
+          content: '';
+          display: inline-block;
+          background-image: url(/header-bg.png);
+          background-position: bottom center;
+          background-size: 100%;
+          background-repeat: no-repeat;
+          border-top: 1px solid transparent;
+          margin: 0 auto;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          transform: scaleY(-1);
+        }
       `}
     >
-      <Paragraph light noMargin small>
+      <Paragraph
+        light
+        noMargin
+        small
+        css={css`
+          ${heading.small(theme)};
+          color: #d5f7f0;
+          margin: 2.4rem auto;
+          max-width: ${config.siteContentWidth};
+          position: relative;
+
+          a {
+            box-shadow: none;
+            color: #d5f7f0;
+          }
+        `}
+      >
         Made with{' '}
         <span role="img" aria-label="love">
           ❤️
